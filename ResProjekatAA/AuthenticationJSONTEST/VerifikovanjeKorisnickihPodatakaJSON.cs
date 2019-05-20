55d06af6d480e793faaaae54f8bff880c4d1a1a7
marcodeee <marcodeee@gmail.com>
2019-05-20 21:25:14 +0200

    test - prilikom verifikovanja da li je neki od arguemnta null JSON

---
+++ ResProjekatAA/AuthenticationJSONTEST/VerifikovanjeKorisnickihPodatakaJSON.cs
@@ -0,0 +1,31 @@
+﻿using AuthenticationJSON;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthenticationJSONTEST
+{
+    [TestFixture]
+    public class VerifikovanjeKorisnickihPodatakaJSON
+    {
+        [Test]
+        [TestCase(null, "456")]
+        [TestCase("pera", null)]
+        [TestCase(null, null)]
+
+        public void VerifikovanjeKorisnickihPodatakaJSON_NullArguments_ThrowsException(string username, string lozinka)
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+
+            {
+                AuthenticationInterfaceJSON vj = new AuthenticationInterfaceJSON();
+                vj.VerifikovanjeKorisnickihPodatakaJSON(username, lozinka);
+            }
+            );
+        }
+        
+    }
+}
